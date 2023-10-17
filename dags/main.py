036480e366c471c48a99efbcd71132ac0c92d3dc
MuhammadMuhidin <muhammadmuhidin@users.noreply.github.com>
2023-10-17 13:22:35 +0700

    takeout unused variable

--- dags/main.py
+++ dags/main.py
@@ -1,3 +1,4 @@
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from lib import Notification, FakerGenerators
 from process import Extract, Load
