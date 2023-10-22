aa157d0b6cec6a4d826d66d442b425d056e0d017
MuhammadMuhidin <muhammadmuhidin@users.noreply.github.com>
2023-10-22 18:57:43 +0700

    make it idempoten

--- dags/lib/faker1000.py
+++ dags/lib/faker1000.py
@@ -3,129 +3,140 @@ import polars as pl
 import json
 
 class FakerGenerators:
-    # Initialize number of records to generate
+    '''
+    Class to generate fake data, such as users, user_events, user_transactions, facebook_ads, instagram_ads.
+    with the help of Faker generate 1000 rows of data.
+    output : csv, parquet
+    '''
     def __init__(self, num_records=1000):
         self.fake = Faker()
         self.num_records = num_records
 
+    def _generate_data(self, fields, data_function):
+        # Create an empty dictionary
+        data = {field: [] for field in fields}
+
+        # Generate data for each field
+        for _ in range(self.num_records):
+            record_data = data_function()
+            for field, value in record_data.items():
+                data[field].append(value)
+
+        # Return the generated data        
+        return data
+
+    def _write_csv(self, data, filename):
+        # Write the data to a CSV file
+        df = pl.DataFrame(data)
+        df.write_csv(filename)
+
+    def _write_parquet(self, data, filename):
+        # Write the data to a parquet file
+        df = pl.DataFrame(data)
+        df.write_parquet(filename)
+
     def generate_users(self):
-        # Create a dictionary to store the data
-        data = {
-            'id': [],
-            'first_name': [],
-            'last_name': [],
-            'email': [],
-            'dob': [],
-            'gender': [],
-            'register_date': [],
-            'client_id': []
-        }
+        # Define the fields to generate
+        fields = ['id', 'first_name', 'last_name', 'email', 'dob', 'gender', 'register_date', 'client_id']
 
-        # Generate the data and append it to the dictionary
-        for _ in range(self.num_records):
-            data['id'].append(self.fake.unique.pyint())
-            data['first_name'].append(self.fake.first_name())
-            data['last_name'].append(self.fake.last_name())
-            data['email'].append(self.fake.email())
-            data['dob'].append(self.fake.date_of_birth(tzinfo=None, minimum_age=18, maximum_age=50)) # Scope age 18-50
-            data['gender'].append(self.fake.random_element(elements=('Male', 'Female')))
-            data['register_date'].append(self.fake.date_this_century(before_today=True))
-            data['client_id'].append(self.fake.pyint())
-
-        # Write the data to a parquet file
-        df = pl.DataFrame(data)
-        df.write_parquet('/data/raw/users.parquet')
-    
-    def generate_user_events(self):
-        data = {
-           'id': [],
-           'user_id': [],
-           'event_type': [],
-           'timestamp': [],
-           'device_type': [],
-           'location': [],
-           'event_data': []
-           }
-
-        # Generate the data and append it to the dictionary
-        for _ in range(self.num_records):
-           data['id'].append(self.fake.unique.pyint())
-           data['user_id'].append(self.fake.pyint())
-           data['event_type'].append(self.fake.word())
-           data['timestamp'].append(self.fake.date_time_this_decade(before_now=True, after_now=False).strftime("%Y-%m-%d %H:%M:%S"))
-           event_data = {'device_type': self.fake.random_element(elements=('Mobile', 'Desktop', 'Tablet')),
-                        'location': self.fake.city(), 'event_type': self.fake.random_element(elements=('login', 'search', 'purchase'))}
-           data['device_type'].append(event_data['device_type'])
-           data['location'].append(event_data['location'])
-           data['event_data'].append(json.dumps(event_data))
-
-        # Write the data to a parquet file
-        df = pl.DataFrame(data)
-        df.write_parquet('/data/raw/user_events.parquet')
-
-    def generate_user_transactions(self):
-        # Create a dictionary to store the data
-        data = {
-        'id': [],
-        'user_id': [],
-        'transaction_date': [],
-        'amount': [],
-        'transaction_type': 'purchase',
-        }
-
-        # Generate the data and append it to the dictionary
-        for _ in range(self.num_records):
-            data['id'].append(self.fake.unique.pyint())
-            data['user_id'].append(self.fake.pyint())
-            data['transaction_date'].append(self.fake.date_between(start_date='-1y', end_date='today')) # Scope 1 year ago - today
-            data['amount'].append(self.fake.pyint())
-
-        # Write the data to a parquet file
-        df = pl.DataFrame(data)
-        df.write_parquet('/data/raw/user_transactions.parquet')
-
-    def generate_facebook_ads(self):
-        data = {
-            'id': [],
-            'ads_id': [],
-            'device_type': [],
-            'device_id': [],
-            'timestamp': [],
+        # Define a function to generate users data
+        def data_function():
+            return {
+                'id': self.fake.unique.pyint(),
+                'first_name': self.fake.first_name(),
+                'last_name': self.fake.last_name(),
+                'email': self.fake.email(),
+                'dob': self.fake.date_of_birth(tzinfo=None, minimum_age=18, maximum_age=50),
+                'gender': self.fake.random_element(elements=('Male', 'Female')),
+                'register_date': self.fake.date_this_century(before_today=True),
+                'client_id': self.fake.pyint()
             }
 
-        # Generate the data and append it to the dictionary
-        for _ in range(self.num_records):
-            data['id'].append(self.fake.unique.pyint())
-            data['ads_id'].append(self.fake.random_element(elements=('lebaran_campaign', 'imlek_campaign', 'natal_campaign')))
-            data['device_type'].append(self.fake.random_element(elements=('Desktop', 'Android', 'IOS')))
-            data['device_id'].append(self.fake.ipv4())
-            data['timestamp'].append(self.fake.date_time())
+        # Generate user data
+        data = self._generate_data(fields, data_function)
+        self._write_parquet(data, '/data/raw/users.parquet')
 
-        # Write the data to a parquet file
-        df = pl.DataFrame(data)
-        df.write_csv('/data/raw/facebook_ads.csv')
+    def generate_user_events(self):
+        # Define the fields to generate
+        fields = ['id', 'user_id', 'event_type', 'timestamp', 'device_type', 'location', 'event_data']
+
+        # Define a function to generate user_events data
+        def data_function():
+            # Define the event data
+            event_data = {
+                'device_type': self.fake.random_element(elements=('Mobile', 'Desktop', 'Tablet')),
+                'location': self.fake.city(),
+                'event_type': self.fake.random_element(elements=('login', 'search', 'purchase'))
+            }
+
+            # Return the event data
+            return {
+                'id': self.fake.unique.pyint(),
+                'user_id': self.fake.pyint(),
+                'event_type': self.fake.word(),
+                'timestamp': self.fake.date_time_this_decade(before_now=True, after_now=False).strftime("%Y-%m-%d %H:%M:%S"),
+                'device_type': event_data['device_type'],
+                'location': event_data['location'],
+                'event_data': json.dumps(event_data)
+            }
+
+        # Generate user_events data
+        data = self._generate_data(fields, data_function)
+        self._write_parquet(data, '/data/raw/user_events.parquet')
+
+    def generate_user_transactions(self):
+        # Define the fields to generate
+        fields = ['id', 'user_id', 'transaction_date', 'amount', 'transaction_type']
+
+        # Define a function to generate user_transactions data
+        def data_function():
+            return {
+                'id': self.fake.unique.pyint(),
+                'user_id': self.fake.pyint(),
+                'transaction_date': self.fake.date_between(start_date='-1y', end_date='today'),
+                'amount': self.fake.pyint(),
+                'transaction_type': 'purchase'
+            }
+
+        # Generate user_transactions data
+        data = self._generate_data(fields, data_function)
+        self._write_parquet(data, '/data/raw/user_transactions.parquet')
+
+    def generate_facebook_ads(self):
+        # Define the fields to generate
+        fields = ['id', 'ads_id', 'device_type', 'device_id', 'timestamp']
+
+        # Define a function to generate facebook_ads data
+        def data_function():
+            return {
+                'id': self.fake.unique.pyint(),
+                'ads_id': self.fake.random_element(elements=('lebaran_campaign', 'imlek_campaign', 'natal_campaign')),
+                'device_type': self.fake.random_element(elements=('Desktop', 'Android', 'IOS')),
+                'device_id': self.fake.ipv4(),
+                'timestamp': self.fake.date_time()
+            }
+
+        # Generate facebook_ads data
+        data = self._generate_data(fields, data_function)
+        self._write_csv(data, '/data/raw/facebook_ads.csv')
 
     def generate_instagram_ads(self):
-        # Create a dictionary to store the data
-        data = {
-            'id': [],
-            'ads_id': [],
-            'device_type': [],
-            'device_id': [],
-            'timestamp': [],
-        }
+        # Define the fields to generate
+        fields = ['id', 'ads_id', 'device_type', 'device_id', 'timestamp']
 
-        # Generate the data and append it to the dictionary
-        for _ in range(self.num_records):
-            data['id'].append(self.fake.unique.pyint())
-            data['ads_id'].append(self.fake.random_element(elements=('lebaran_campaign', 'imlek_campaign', 'natal_campaign')))
-            data['device_type'].append(self.fake.random_element(elements=('Desktop', 'Android', 'IOS')))
-            data['device_id'].append(self.fake.ipv4())
-            data['timestamp'].append(self.fake.date_time())
+        # Define a function to generate instagram_ads data
+        def data_function():
+            return {
+                'id': self.fake.unique.pyint(),
+                'ads_id': self.fake.random_element(elements=('lebaran_campaign', 'imlek_campaign', 'natal_campaign')),
+                'device_type': self.fake.random_element(elements=('Desktop', 'Android', 'IOS')),
+                'device_id': self.fake.ipv4(),
+                'timestamp': self.fake.date_time()
+            }
 
-        # Write the data to a parquet file
-        df = pl.DataFrame(data)
-        df.write_csv('/data/raw/instagram_ads.csv')
+        # Generate instagram_ads data
+        data = self._generate_data(fields, data_function)
+        self._write_csv(data, '/data/raw/instagram_ads.csv')
 
     @staticmethod
     def create():
@@ -137,3 +148,10 @@ class FakerGenerators:
       fake.generate_user_transactions()
       fake.generate_facebook_ads()
       fake.generate_instagram_ads()
+
+def main():
+    # Unit Testing Purpose
+    fake = FakerGenerators()
+    fake.create()
+if __name__== "__main__":
+    main ()
