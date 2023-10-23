eaa8c30e5387660287a38e268968e9d9bd115566
MuhammadMuhidin <muhammadmuhidin@users.noreply.github.com>
2023-10-23 12:58:42 +0700

    add and adjust dim_table/fact_table

--- dags/sql/create_dim_facts.sql
+++ dags/sql/create_dim_facts.sql
@@ -53,6 +53,44 @@ this query creates a dimension table (dim_ads) that consolidates advertising dat
 It's likely designed to provide a unified view of advertising data for analytics or reporting purposes.
 */
 
+-- Drop table if exists and create table dim_transactions;
+DROP TABLE IF EXISTS warehouse.dim_transactions;
+CREATE TABLE warehouse.dim_transactions AS
+select
+	t.user_id,
+	t.transaction_date,
+	t.amount,
+    t.transaction_type
+FROM user_transactions t;
+/*
+this query is used to create a dimension table (dim_transactions) that consolidates transaction data from the user_transactions table.
+*/
+
+-- Drop table if exists and create table dim_events;
+DROP TABLE IF EXISTS warehouse.dim_events;
+CREATE TABLE warehouse.dim_events AS
+SELECT
+    *
+FROM user_events;
+/*
+this query is used to create a dimension table (dim_events) that consolidates event data from the user_events table.
+*/
+
+-- Drop table if exists and create table dim_date;
+DROP TABLE IF EXISTS warehouse.dim_date;
+CREATE TABLE warehouse.dim_date AS
+SELECT
+    timestamp::date as date_key,
+    EXTRACT(DOW FROM timestamp::date) AS day_of_week,
+    EXTRACT(WEEK FROM timestamp::date) AS week,
+    EXTRACT(MONTH FROM timestamp::date) AS month,
+    EXTRACT(QUARTER FROM timestamp::date) AS quarter,
+    EXTRACT(YEAR FROM timestamp::date) AS year
+FROM user_events;
+/*
+this query is used to create a dimension table (dim_date) that consolidates date data from the user_events table.
+*/
+
 -- Drop table if exists and create table fact_ads_performance;
 DROP TABLE IF EXISTS warehouse.fact_ads_performance;
 CREATE TABLE warehouse.fact_ads_performance AS
@@ -61,16 +99,11 @@ SELECT
     COUNT(DISTINCT a.id) AS total_clicks,
     COUNT(DISTINCT CASE WHEN a.ads_source = 'Facebook' THEN a.id END) AS total_facebook_clicks,
     COUNT(DISTINCT CASE WHEN a.ads_source = 'Instagram' THEN a.id END) AS total_instagram_clicks,
-    COUNT(DISTINCT CASE WHEN u.id IS NOT NULL THEN a.id END) AS total_converted,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN 1 ELSE 0 END) AS total_purchases,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN t.amount ELSE 0 END) AS total_purchase_amount
-FROM (
-    SELECT id, ads_id, 'Facebook' AS ads_source FROM facebook_ads
-    UNION ALL
-    SELECT id, ads_id, 'Instagram' AS ads_source FROM instagram_ads
-) AS a
-LEFT JOIN users AS u ON a.id = u.client_id
-LEFT JOIN user_transactions AS t ON u.id = t.user_id AND t.transaction_type = 'purchase'
+FROM warehouse.dim_ads a
+LEFT JOIN warehouse.dim_user u ON a.id = u.client_id
+LEFT JOIN warehouse.dim_transactions t ON u.user_id = t.user_id
 GROUP BY a.ads_id;
 /*
 this query is designed to create a fact table that summarizes the performance of ads by aggregating various metrics such as total clicks,
@@ -83,8 +116,8 @@ of advertising campaigns.
 DROP TABLE IF EXISTS warehouse.fact_user_performance;
 CREATE TABLE warehouse.fact_user_performance AS
 SELECT
-    u.id AS user_id,
-    u.first_name || ' ' || u.last_name AS user_name,
+    u.user_id,
+    u.full_name,
     MAX(e.timestamp)::date AS last_login,
     MAX(e.timestamp)::date AS last_activity,
     COUNT(DISTINCT e.id) AS total_events,
@@ -92,32 +125,34 @@ SELECT
     COUNT(DISTINCT CASE WHEN e.event_type = 'search' THEN e.id END) AS total_searches,
     COUNT(DISTINCT CASE WHEN e.event_type = 'purchase' THEN e.id END) AS total_purchases,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN t.amount ELSE 0 END) AS total_purchase_amount
-FROM users u
-LEFT JOIN user_events e ON u.id = e.user_id
-LEFT JOIN user_transactions t ON u.id = t.user_id
+FROM warehouse.dim_user  u
+LEFT JOIN warehouse.dim_events e ON u.user_id = e.user_id
+LEFT JOIN warehouse.dim_transactions t ON u.user_id = t.user_id
 WHERE e.timestamp IS NOT NULL
-GROUP BY u.id, user_name;
+GROUP BY u.user_id, full_name;
 /*
 this query is designed to create a fact table that provides a consolidated view of user performance metrics,
 including last login and activity dates, total events, count of specific event types (login, search, purchase), and total purchase amount.
 The resulting fact_user_performance table is likely intended for use in analytics and reporting to analyze user engagement and behavior.
 */
 
+
 -- Drop table if exists and create table fact_daily_event_performance;
 DROP TABLE IF EXISTS warehouse.fact_daily_event_performance;
 CREATE TABLE warehouse.fact_daily_event_performance AS
 SELECT
-    e.timestamp::date AS event_date,
+    dd.date_key AS event_date,
     COUNT(DISTINCT e.id) AS total_events,
     COUNT(DISTINCT CASE WHEN e.event_type = 'login' THEN e.id END) AS total_logins,
     COUNT(DISTINCT CASE WHEN e.event_type = 'logout' THEN e.id END) AS total_logouts,
     COUNT(DISTINCT CASE WHEN e.event_type = 'search' THEN e.id END) AS total_searches,
-    COUNT(DISTINCT u.id) AS total_users,
-    COUNT(DISTINCT CASE WHEN t.transaction_type = 'purchase' THEN u.id END) AS total_purchasing_users,
+    COUNT(DISTINCT u.user_id) AS total_users,
+    COUNT(DISTINCT CASE WHEN t.transaction_type = 'purchase' THEN u.user_id END) AS total_purchasing_users,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN t.amount ELSE 0 END) AS total_purchase_amount
-FROM user_events e
-LEFT JOIN users u ON e.user_id = u.id
-LEFT JOIN user_transactions t ON u.id = t.user_id
+FROM warehouse.dim_events e
+LEFT JOIN warehouse.dim_user u ON e.user_id = u.user_id
+LEFT JOIN warehouse.dim_transactions t ON u.user_id = t.user_id
+left join warehouse.dim_date dd on cast(e.timestamp as date) = dd.date_key 
 GROUP BY event_date
 ORDER BY event_date;
 /*
@@ -131,22 +166,19 @@ and reporting to analyze event engagement and behavior.
 DROP TABLE IF EXISTS warehouse.fact_weekly_ads_performance;
 CREATE TABLE warehouse.fact_weekly_ads_performance AS
 SELECT
-    DATE_TRUNC('week', CAST(a.timestamp AS timestamp)) AS week_start,
+    dd.week,
     a.ads_id,
     COUNT(DISTINCT a.id) AS total_clicks,
-    COUNT(DISTINCT CASE WHEN u.id IS NOT NULL THEN a.id END) AS total_converted_users,
+    COUNT(DISTINCT CASE WHEN u.user_id IS NOT NULL THEN a.id END) AS total_converted_users,
     COUNT(DISTINCT CASE WHEN a.ads_source = 'Facebook' THEN a.id END) AS total_facebook_clicks,
     COUNT(DISTINCT CASE WHEN a.ads_source = 'Instagram' THEN a.id END) AS total_instagram_clicks,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN 1 ELSE 0 END) AS total_purchases,
     SUM(CASE WHEN t.transaction_type = 'purchase' THEN t.amount ELSE 0 END) AS total_purchase_amount
-FROM (
-    SELECT id, ads_id, 'Facebook' AS ads_source, timestamp FROM facebook_ads
-    UNION ALL
-    SELECT id, ads_id, 'Instagram' AS ads_source, timestamp FROM instagram_ads
-) AS a
-LEFT JOIN users AS u ON a.id = u.client_id
-LEFT JOIN user_transactions AS t ON u.id = t.user_id AND t.transaction_type = 'purchase'
-GROUP BY week_start, a.ads_id;
+FROM warehouse.dim_ads a
+LEFT JOIN warehouse.dim_user u ON a.id = u.client_id
+LEFT JOIN warehouse.dim_transactions t ON u.user_id = t.user_id
+inner join warehouse.dim_date dd on t.transaction_date = dd.date_key 
+GROUP BY dd.week, a.ads_id;
 /*
 this query is designed to create a fact table that provides a weekly summary of advertising performance metrics,
 including total clicks, converted users, clicks by source (Facebook/Instagram), total purchases, and total purchase amount.
