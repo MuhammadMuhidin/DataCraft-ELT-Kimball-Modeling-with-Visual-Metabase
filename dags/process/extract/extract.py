a32bc988001f1fead2434d6960d0a5b11a7e77ca
MuhammadMuhidin <muhammadmuhidin@users.noreply.github.com>
2023-10-24 22:24:15 +0700

    move to csv type

--- dags/process/extract/extract.py
+++ dags/process/extract/extract.py
@@ -2,20 +2,24 @@ import polars as pl
 import os
 
 class Extract:
+    '''
+    This class is used to extract data from raw folder to extracted folder,
+    then export to csv
+    '''
     def __init__(self, DATA_PATH):
         # initialize path
         self.path = DATA_PATH
 
     def extract_processing(self):
-        # seacrh file in raw folder then extract to extracted with json format
+        # seacrh file in raw folder then extract to extracted with csv format
         for filename in os.listdir(self.path+'/raw'):
             if filename.endswith('.csv'):
                 csv_file_path = os.path.join(self.path+'/raw', filename)
                 df = pl.read_csv(csv_file_path)
-                extract_to = os.path.join(self.path+'/extracted', filename[:-4]+'.json')
-                df.write_json(extract_to)
+                extract_to = os.path.join(self.path+'/extracted', filename[:-4]+'.csv')
+                df.write_csv(extract_to)
             elif filename.endswith('.parquet'):
                 parquet_file_path = os.path.join(self.path+'/raw', filename)
                 df = pl.read_parquet(parquet_file_path)
-                extract_to = os.path.join(self.path+'/extracted', filename[:-8]+'.json')
-                df.write_json(extract_to)
+                extract_to = os.path.join(self.path+'/extracted', filename[:-8]+'.csv')
+                df.write_csv(extract_to)
